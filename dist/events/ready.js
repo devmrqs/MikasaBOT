81040d67643a3761872e68e1cc824bde0b9eeb54
over <overboboto@gmail.com>
2026-09-17 00:40:39 -0300

    chore: remove dist do controle de versao

--- dist/events/ready.js
+++
@@ -1,9 +0,0 @@
-const event = {
-    name: "clientReady",
-    once: true,
-    execute(client) {
-        console.log(`[bot] logado como ${client.user.tag}`);
-    },
-};
-export default event;
-//# sourceMappingURL=ready.js.map
